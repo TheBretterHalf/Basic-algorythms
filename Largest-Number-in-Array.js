fe978492d2130eb376c2e0dc6f6acaa6d3103121
Brett Stainken <brettstainken.student@careerdevs.com>
2018-09-12 13:15:02 +0000

    Largest-Array

---
+++ Largest-Number-in-Array.js
@@ -0,0 +1,9 @@
+function largestOfFour(arr) {
+  // You can do this!
+  var arr1=arr.splice(0,1);
+  var largestNumber=""
+
+  console.log(arr1);
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
