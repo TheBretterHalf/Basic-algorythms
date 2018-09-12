498e16bd91027b4b91a5fc13a9dfa4eb2477c38a
Brett Stainken <brettstainken.student@careerdevs.com>
2018-09-12 15:04:19 +0000

    Final-Largest-Num

--- Largest-Number-in-Array.js
+++ Largest-Number-in-Array.js
@@ -1,9 +1,20 @@
 function largestOfFour(arr) {
   // You can do this!
-  var arr1=arr.splice(0,1);
-  var largestNumber=""
-
-  console.log(arr1);
+  var bigNum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      var arr = arr.splice(i, 4, j);
+        if (arr[i][j]>bigNum){
+          bigNum=arr[i][j];
+        }
+    }
+  }
+   console.log(bigNum);
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1]
+]);
