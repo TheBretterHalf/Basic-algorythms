347320bbcbee41a224479191d35cdb31029a8062
Brett Stainken <brettstainken.student@careerdevs.com>
2018-09-18 00:03:19 +0000

    Where do I Belong ifstatement

--- WhereDoIBelong.js
+++ WhereDoIBelong.js
@@ -1,9 +1,11 @@
 function getIndexToIns(arr, num) {
-    for (let i>0;i<arr.length;i++){
-        
-    }
+    for (let i=0; i++; i<arr.length){
+        if (arr[i]<num && arr[i+1]>num){
+        console.log(arr[i], arr[i+1])
+        }
   // Find my place in this sorted array.
   return num;
+    }
 }
 
-getIndexToIns([40, 60], 50);
+getIndexToIns([40, 60, 70, 80], 50);
