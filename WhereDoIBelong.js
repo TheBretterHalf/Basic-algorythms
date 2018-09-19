3ff82a315ae2a822f608f544f92db93411c7c48d
Brett Stainken <brettstainken.student@careerdevs.com>
2018-09-19 00:27:24 +0000

    Where do I Belong runnwiki

--- WhereDoIBelong.js
+++ WhereDoIBelong.js
@@ -2,7 +2,7 @@ function getIndexToIns(arr, num) {
     for (let i=0; i++; i<arr.length){
         if (arr[i]<num && arr[i+1]>num){
         console.log(arr[i], arr[i+1])
-        }
+        }else(console.log("Wrong"))
   // Find my place in this sorted array.
   return num;
     }
