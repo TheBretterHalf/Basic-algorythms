d788beb5c4592cc622e7a116f072402cc42ca5a7
Brett Stainken <brettstainken.student@careerdevs.com>
2018-09-25 22:40:30 +0000

    While-for-loop

--- Stand-in-line.js
+++ Stand-in-line.js
@@ -1,8 +1,9 @@
 function nextInLine(arr, item) {
   // Your code here
     arr=nextInLine.split([],5);
-  console.log(arr);  // Change this line
-}
 
+  console.log(arr);  // Change this line
+
+}
 // Test Setup
 var testArr = [1,2,3,4,5];
---
+++ While-Loop.js
@@ -0,0 +1,6 @@
+var cohort5Names=["Paul", "Dan", "Brett", "Josh", "Tom", "Christina", "Joe", "Mick", "Melissa", "Nathan", "Alejandra"]
+var arr=[""];
+    for (let i=0; i<cohort5Names.length; i++){
+        var arr="Hello "+cohort5Names[i]+"?";
+    console.log(arr)
+    }
