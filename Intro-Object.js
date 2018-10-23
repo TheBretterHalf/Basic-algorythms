ad703c7c52353e65093a9cae9ef3094213259ea1
Brett Stainken <brettstainken.student@careerdevs.com>
2018-10-23 22:15:41 +0000

    Algors

---
+++ Intro-Object.js
@@ -0,0 +1,17 @@
+var obj={
+    Monica: "In my Life",
+    Erica: "By my Side",
+    Rita: "All I Need",
+    Tina: "What I See",
+    Sandra: "In the Sun",
+    Mary: "All night Long",
+    Jessica:"Here I am",
+    You:"Makes me your Man",
+    
+    function(){
+        for (var i=0; i>obj.length; i++){
+            console.log("A little bit of " + obj[i])
+        }
+            
+    }
+}
