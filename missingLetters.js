6b598050c61cbe17d4d53b2790b1fe857194c3f6
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-20 22:50:03 +0000

    C to F

---
+++ missingLetters.js
@@ -0,0 +1,8 @@
+function fearNotLetter(str) {
+    var alphabet=("abcdefghijklmnopqrstuvwxyz")
+    var spalphabet=alphabet.split("")
+    var spstr=str.split("")
+    console.log(spstr)
+}
+
+fearNotLetter("abce"); 
