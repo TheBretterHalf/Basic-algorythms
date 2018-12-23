102b1537c448a105cdb828703e3d568f821a6042
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-23 23:31:55 +0000

    summing primes i,j

--- SummingAllPrimes3.js
+++ SummingAllPrimes3.js
@@ -1,1 +1,13 @@
 //summing all primes
+function isprime(num){
+    for (let i=0; i<num; i++){
+        for(let j=1; j<num; j++){
+            if(i%j == 0 ){
+                console.log(i)
+            }else{
+                console.log(false,i)
+            }
+        }
+    }
+}
+isprime(15)
