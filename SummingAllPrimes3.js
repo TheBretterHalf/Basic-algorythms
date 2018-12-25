ad2c384cb0e87acc59be4c25618eff4b7c2f7b67
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-25 23:55:53 +0000

    Foreshadow

--- SummingAllPrimes3.js
+++ SummingAllPrimes3.js
@@ -1,13 +1,19 @@
 //summing all primes
 function isprime(num){
     for (let i=0; i<num; i++){
-        for(let j=1; j<num; j++){
-            if(i%j == 0 ){
-                console.log(i)
+        //let i be divided by previous iterations of i using joe's fibb series
+        //make sure i cannot be divided by even number to cut down on the callouts
+        //find series depection of prime numbers
+        for(let j=1; j<i; j++){
+            //disp all nums and false for those evenly divisible
+            if(i%j == 0){
+                //return
             }else{
-                console.log(false,i)
+                console.log(true, i, j)
             }
         }
     }
 }
 isprime(15)
+//adad
+//collect all values
