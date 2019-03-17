05d0ea69728f5e34db5947086e0e8d4acbc7b638
Brett Stainken <brettstainken.student@careerdevs.com>
2019-03-17 20:43:43 +0000

    debugs

--- MissingLetters.js
+++ MissingLetters.js
@@ -6,18 +6,7 @@ function fearNotLetter(string) {
   console.log(letters)
   console.log(str)
   for (let i=0; i++; i<alphabet.length) {
-      if (letters[i]==str[i]){
-          for (let j=0; j++; j<string.length){
-              console.log(str[j])
-              if (letters[i+j]==str[j]){
-              } else {
-                  var result =(letters[i+j])
-                  console.log(result)
-              }
-          }
-      } else {
-          console.log("Try Again")
-      }
+
   }
 }
 
@@ -29,3 +18,17 @@ fearNotLetter("abce");
 //-if else statement running the error
 //log missing letter to console
 //delete 4 loops and debug
+
+//debugging 
+    //   if (letters[i]==str[i]){
+    //       for (let j=0; j++; j<string.length){
+    //           console.log(str[j])
+    //           if (letters[i+j]==str[j]){
+    //           } else {
+    //               var result =(letters[i+j])
+    //               console.log(result)
+    //           }
+    //       }
+    //   } else {
+    //       console.log("Try Again")
+    //   }
