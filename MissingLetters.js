26caa9895a696fcabf7aab7a91e1142080a70933
Brett Stainken <brettstainken.student@careerdevs.com>
2019-03-24 16:59:17 +0000

    Nested4loops

--- MissingLetters.js
+++ MissingLetters.js
@@ -28,3 +28,4 @@ fearNotLetter("abce");
 //-run through alphabet, starting with first letter of str
 //-if else statement running the error
 //log missing letter to console
+//delete 4 loops and debug
