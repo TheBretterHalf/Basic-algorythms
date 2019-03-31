a58ed8f4d2e01e73b8c700ebf6ae23354c420546
Brett Stainken <brettstainken.student@careerdevs.com>
2019-03-31 16:24:39 +0000

    Missing Letters Pseudo

--- MissingLetters.js
+++ MissingLetters.js
@@ -1,11 +1,17 @@
 
-function fearNotLetter(str) {
+function fearNotLetter(string) {
   var alphabet = 'abcdefghijklmnopqrstuvwxyz'
   var letters = alphabet.split('')
-  var i=0
-  for (i=0; i++; i<letters.length) {
+  var str = string.split('')
+  for (let i=0; i++; i<letters.length) {
       if (letters[i]==str[0]){
-          
+          for (let j=0; j++; j<str.length){
+              if (letters[i+j]==str[j]){
+                  
+              } else {
+                  console.log(letters[i+j])
+              }
+          }
       } else {
           alert ("Try Again")
       }
