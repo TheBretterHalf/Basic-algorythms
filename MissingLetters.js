712fa5e77cd6bacd14d5dfb5d9ed0a42301a098c
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-01 16:18:53 +0000

    Missing Letters Pseudo

--- MissingLetters.js
+++ MissingLetters.js
@@ -1,5 +1,21 @@
+
 function fearNotLetter(str) {
-  return str;
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  var letters = alphabet.split('')
+  var i=0
+  for (i=0; i++; i<letters.length) {
+      if (letters[i]==str[0]){
+          
+      } else {
+          alert ("Try Again")
+      }
+  }
 }
 
 fearNotLetter("abce");
+
+//pseudo
+//-get alphabet into single letters
+//-run through alphabet, starting with first letter of str
+//-if else statement running the error
+//log missing letter to console
